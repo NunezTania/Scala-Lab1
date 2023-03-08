3c8a160f249a71da67b37324ddc2d1098df32649
magskwa <magali.egger@heig-vd.ch>
2023-03-08 10:55:55 +0100

    all tests pass

--- src/main/scala/Chat/Tokenized.scala
+++ src/main/scala/Chat/Tokenized.scala
@@ -4,13 +4,16 @@ import Chat.Token.*
 import Utils.SpellCheckerService
 
 trait Tokenized:
-  /**
-    * Get the next token of the user input, or EOL if there is no more token.
-    * @return a tuple that contains the string value of the current token, and the identifier of the token
+  /** Get the next token of the user input, or EOL if there is no more token.
+    * @return
+    *   a tuple that contains the string value of the current token, and the
+    *   identifier of the token
     */
   def nextToken(): (String, Token)
 
 class TokenizedImpl(val tokens: Array[(String, Token)]) extends Tokenized:
-  // TODO - Part 1 Step 3
-  def nextToken(): (String, Token) = ???
+// TODO - Part 1 Step 3
+  def nextToken(): (String, Token) =
+    return ("test", Token.EOL)
+
 end TokenizedImpl
--- src/main/scala/Utils/SpellCheckerService.scala
+++ src/main/scala/Utils/SpellCheckerService.scala
@@ -1,38 +1,51 @@
 package Utils
 
 trait SpellCheckerService:
-  /**
-    * This dictionary is a Map object that contains valid words as keys and their normalized equivalents as values (e.g.
-    * we want to normalize the words "veux" and "aimerais" in one unique term: "vouloir").
+  /** This dictionary is a Map object that contains valid words as keys and
+    * their normalized equivalents as values (e.g. we want to normalize the
+    * words "veux" and "aimerais" in one unique term: "vouloir").
     */
   val dictionary: Map[String, String]
 
-  /**
-    * Calculate the Levenstein distance between two words.
-    * @param s1 the first word
-    * @param s2 the second word
-    * @return an integer value, which indicates the Levenstein distance between "s1" and "s2"
+  /** Calculate the Levenstein distance between two words.
+    * @param s1
+    *   the first word
+    * @param s2
+    *   the second word
+    * @return
+    *   an integer value, which indicates the Levenstein distance between "s1"
+    *   and "s2"
     */
   def stringDistance(s1: String, s2: String): Int
 
-  /**
-    * Get the syntactically closest word in the dictionary from the given misspelled word, using the "stringDistance"
-    * function. If the word is a number or a pseudonym, this function just returns it.
-    * @param misspelledWord the mispelled word to correct
-    * @return the closest normalized word from "mispelledWord"
+  /** Get the syntactically closest word in the dictionary from the given
+    * misspelled word, using the "stringDistance" function. If the word is a
+    * number or a pseudonym, this function just returns it.
+    * @param misspelledWord
+    *   the mispelled word to correct
+    * @return
+    *   the closest normalized word from "mispelledWord"
     */
   def getClosestWordInDictionary(misspelledWord: String): String
 end SpellCheckerService
 
-class SpellCheckerImpl(val dictionary: Map[String, String]) extends SpellCheckerService:
+class SpellCheckerImpl(val dictionary: Map[String, String])
+    extends SpellCheckerService:
   // TODO - Part 1 Step 2
   def stringDistance(s1: String, s2: String): Int =
     val l1 = s1.length()
     val l2 = s2.length()
     if l1.min(l2) == 0 then l1.max(l2)
-    else if s1.charAt(0) == s2.charAt(0) then stringDistance(s1.substring(1), s2.substring(1))
-    else 1 + stringDistance(s1.substring(1), s2).min(stringDistance(s1, s2.substring((1)))).min(stringDistance(s1.substring(1), s2.substring(1)))
+    else if s1.charAt(0) == s2.charAt(0) then
+      stringDistance(s1.substring(1), s2.substring(1))
+    else
+      1 + stringDistance(s1.substring(1), s2)
+        .min(stringDistance(s1, s2.substring((1))))
+        .min(stringDistance(s1.substring(1), s2.substring(1)))
 
   // TODO - Part 1 Step 2
-  def getClosestWordInDictionary(misspelledWord: String): String = ???
+  def getClosestWordInDictionary(misspelledWord: String): String =
+    if misspelledWord.matches("[0-9]+") || misspelledWord.startsWith("_") then
+      misspelledWord
+    else dictionary.keys.minBy(stringDistance(misspelledWord, _))
 end SpellCheckerImpl

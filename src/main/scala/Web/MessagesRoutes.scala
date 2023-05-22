3f0f66b02113bbe212f2fe8c609c788ca9c94eba
magskwa <magali.egger@heig-vd.ch>
2023-05-22 19:51:42 +0200

    clean

--- src/main/scala/Web/MessagesRoutes.scala
+++ src/main/scala/Web/MessagesRoutes.scala
@@ -27,30 +27,15 @@ class MessagesRoutes(
     extends cask.Routes:
   import Decorators.getSession
 
+  var openConnections = Set.empty[cask.WsChannelActor]
+
   @getSession(
     sessionSvc
   ) // This decorator fills the `(session: Session)` part of the `index` method.
   @cask.get("/")
   def index()(session: Session) = {
-    // TODO - Part 3 Step 2: Display the home page (with the message board and the form to send new messages)
     Layouts.homePage(msgSvc.getLatestMessages(20), None)
   }
-  // session.getCurrentUser.map(u => s"You are logged in as ${u} !")
-  //      .getOrElse("You are not logged in !")
-
-  // TODO - Part 3 Step 4b: Process the new messages sent as JSON object to `/send`. The JSON looks
-  //      like this: `{ "msg" : "The content of the message" }`.
-  //
-  //      A JSON object is returned. If an error occurred, it looks like this:
-  //      `{ "success" : false, "err" : "An error message that will be displayed" }`.
-  //      Otherwise (no error), it looks like this:
-  //      `{ "success" : true, "err" : "" }`
-  //
-  //      The following are treated as error:
-  //      - No user is logged in
-  //      - The message is empty
-  //
-  //      If no error occurred, every other user is notified with the last 20 messages
 
   @getSession(sessionSvc)
   @cask.postJson("/send")
@@ -88,7 +73,7 @@ class MessagesRoutes(
                 ujson.Obj("success" -> false, "err" -> e.getMessage())
           } else {
 
-            // verifier mention
+            // verify other mention
             val mention = if (msg.startsWith("@")) then {
               val mention = msg.split(" ")(0).stripPrefix("@")
               Some(mention)
@@ -108,9 +93,6 @@ class MessagesRoutes(
         )
       }
   }
-
-  // TODO - Part 3 Step 4c: Process and store the new websocket connection made to `/subscribe`
-  var openConnections = Set.empty[cask.WsChannelActor]
 
   @cask.websocket("/subscribe")
   def subscribe() = {
@@ -140,7 +122,6 @@ class MessagesRoutes(
           .map((author, content) => Layouts.message(author, content).toString)
           .reduceLeft(_ + _)
 
-    // concatenate the messages into a single string
     connection.send(
       cask.Ws
         .Text(
@@ -149,19 +130,12 @@ class MessagesRoutes(
     )
   }
 
-  // TODO - Part 3 Step 4d: Delete the message history when a GET is made to `/clearHistory`
-  //
   @getSession(sessionSvc)
   @cask.get("/clearHistory")
   def clearHistory()(session: Session) = {
     msgSvc.deleteHistory()
     Layouts.homePage(List())
   }
-  // TODO - Part 3 Step 5: Modify the code of step 4b to process the messages sent to the bot (message
-  //      starts with `@bot `). This message and its reply from the bot will be added to the message
-  //      store together.
-  //
-  //      The exceptions raised by the `Parser` will be treated as an error (same as in step 4b)
 
   initialize()
 end MessagesRoutes

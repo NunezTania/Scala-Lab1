7d05a3d4176ec08752a6427eeb227d57f5520ec5
magskwa <magali.egger@heig-vd.ch>
2023-06-15 12:17:35 +0200

    prodService future

--- src/main/scala/Data/ProductService.scala
+++ src/main/scala/Data/ProductService.scala
@@ -1,7 +1,11 @@
 package Data
-
+import Chat.ExprTree.Product
 import scala.concurrent.duration.*
 import scala.concurrent.Future
+import Utils.FutureOps
+import scala.util.Failure
+import scala.util.Success
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait ProductService:
   type BrandName = String
@@ -9,20 +13,24 @@ trait ProductService:
 
   def getPrice(product: ProductName, brand: BrandName): Double
   def getDefaultBrand(product: ProductName): BrandName
-  def getPreparationParameters(product: ProductName, brand: BrandName): (Duration, Duration, Double)
+  def getPreparationParameters(
+      product: ProductName,
+      brand: BrandName
+  ): (Duration, Duration, Double)
 
 class ProductImpl extends ProductService:
 
-  var productPrepMap : Map[(String, String), Option[Future[String]]] = Map[(String, String), Option[Future[String]]](
-    ("biere", "boxer") -> None,
-    ("biere","farmer") -> None,
-    ("biere", "wittekop") -> None,
-    ("biere", "punkipa") -> None,
-    ("biere", "jackhammer") -> None,
-    ("biere", "tenebreuse") -> None,
-    ("croissant", "cailler") -> None,
-    ("croissant", "maison") -> None
-  )
+  var productPrepMap: Map[(String, String), Option[Future[Unit]]] =
+    Map[(String, String), Option[Future[Unit]]](
+      ("biere", "boxer") -> None,
+      ("biere", "farmer") -> None,
+      ("biere", "wittekop") -> None,
+      ("biere", "punkipa") -> None,
+      ("biere", "jackhammer") -> None,
+      ("biere", "tenebreuse") -> None,
+      ("croissant", "cailler") -> None,
+      ("croissant", "maison") -> None
+    )
   // TODO - Part 2 Step 2
   def getPrice(product: ProductName, brand: String): Double =
     product match
@@ -40,7 +48,10 @@ class ProductImpl extends ProductService:
           case "cailler" => 2.0
           case "maison"  => 2.0
 
-  def getPreparationParameters(product: ProductName, brand: String): (Duration, Duration, Double) =
+  def getPreparationParameters(
+      product: ProductName,
+      brand: String
+  ): (Duration, Duration, Double) =
     product match
       case "biere" =>
         brand match
@@ -60,5 +71,75 @@ class ProductImpl extends ProductService:
     product match
       case "biere"     => "boxer"
       case "croissant" => "maison"
+
+  def makeProducts(products: List[Product]): List[Future[String]] = {
+    val futures = List()
+
+    for prod <- products do
+
+      val brand = prod.brand.getOrElse(getDefaultBrand(prod.productType))
+
+      val (mean, std, r) = getPreparationParameters(
+        prod.productType,
+        brand
+      )
+      val state = productPrepMap((prod.productType, brand))
+      val count = prod.quantity
+      val (f, nbSucess) = state match
+        case Some(future) =>
+          loopFuture(
+            (mean, std, r),
+            prod.productType,
+            brand,
+            count,
+            future,
+            0
+          )
+        case None => {
+          val future = FutureOps.randomSchedule(mean, std, r)
+          loopFuture(
+            (mean, std, r),
+            prod.productType,
+            brand,
+            count - 1,
+            future,
+            0
+          )
+        }
+
+      // update the map with the new future
+      productPrepMap = productPrepMap + ((prod.productType, brand) -> Some(f))
+
+      val message =
+        if (nbSucess == count) then
+          "All products have been prepared successfully"
+        else if (nbSucess == 0) then "No product has been prepared successfully"
+        else s"Only $nbSucess products have been prepared successfully"
+
+      futures.appended(Future(message))
+    futures
+  }
+
+  def loopFuture(
+      params: (Duration, Duration, Double),
+      prodType: String,
+      brand: String,
+      count: Int,
+      acc: Future[Unit],
+      nbSuccess: Int
+  ): (Future[Unit], Int) =
+    if count == 0 then (acc, nbSuccess)
+    else
+      val f = acc.transformWith {
+        case Success(_) => {
+          var success = nbSuccess + 1
+          FutureOps.randomSchedule(params._1, params._2, params._3)
+        }
+        case Failure(_) => {
+          FutureOps.randomSchedule(params._1, params._2, params._3)
+        }
+      }
+
+      loopFuture(params, prodType, brand, count - 1, f, nbSuccess)
 
 end ProductImpl
